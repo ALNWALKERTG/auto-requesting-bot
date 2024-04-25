0348d0fdddac568616d2d6572642f5f266d63aaf
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-04-25 09:40:34 +0530

    Update base.py

--- plugins/base.py
+++ plugins/base.py
@@ -64,3 +64,48 @@ async def id_handler(client, message:Message):
               f"<b>➲ ɢʀᴏᴜᴘ ɴᴀᴍᴇ:</b> {first}\n<b>➲ ɢʀᴏᴜᴘ ɪᴅ:</b> {grp_id}\n" ,
               quote=True)
 
+@Client.on_message(filters.command('users') & filters.user(ADMINS))
+async def list_users(bot, message):
+    # https://t.me/GetTGLink/4184
+    raju = await message.reply('Getting List Of Users')
+    users = await db.get_all_users()
+    out = "Users Saved In DB Are:\n\n"
+    async for user in users:
+        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
+        if user['ban_status']['is_banned']:
+            out += '( Banned User )'
+        out += '\n'
+    try:
+        await raju.edit_text(out)
+    except MessageTooLong:
+        with open('users.txt', 'w+') as outfile:
+            outfile.write(out)
+        await message.reply_document('users.txt', caption="List Of Users")
+
+@Client.on_message(filters.command('chats') & filters.user(ADMINS))
+async def list_chats(bot, message):
+    raju = await message.reply('Getting List Of chats')
+    chats = await db.get_all_chats()
+    out = "Chats Saved In DB Are:\n\n"
+    async for chat in chats:
+        out += f"**Title:** `{chat['title']}`\n**- ID:** `{chat['id']}`"
+        if chat['chat_status']['is_disabled']:
+            out += '( Disabled Chat )'
+        out += '\n'
+    try:
+        await raju.edit_text(out)
+    except MessageTooLong:
+        with open('chats.txt', 'w+') as outfile:
+            outfile.write(out)
+        await message.reply_document('chats.txt', caption="List Of Chats")
+
+@Client.on_message(filters.command('stats'))
+async def get_stats(bot, message):
+    rju = await message.reply('Fetching stats..')
+    total_users = await db.total_users_count()
+    total_chats = await db.total_chat_count()
+    size = await db.get_db_size()
+    free = 536870912 - size
+    size = get_size(size)
+    free = get_size(free)    
+    await rju.edit(script.STATUS_TXT.format(total_users, total_chats))
