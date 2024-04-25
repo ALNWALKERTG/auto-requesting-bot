a675a1df87b9e31ab4941cc4a4bf274f051105e0
ALNWALKERTG <166110788+ALNWALKERTG@users.noreply.github.com>
2024-04-25 13:33:49 +0530

    Update base.py

--- plugins/base.py
+++ plugins/base.py
@@ -6,7 +6,7 @@ from info import ADMINS
 import logging, re, asyncio, time, shutil, psutil, os, sys
 from utils import get_size, temp
 
-@Client.on_message(filters.command("id"))
+@Client.on_message(filters.command('id'))
 async def id_handler(client, message:Message):
       chat_type = message.chat.type
       if chat_type in [enums.ChatType.PRIVATE]:
